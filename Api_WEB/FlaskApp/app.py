618e3629f6f787640e04d28aa25d428ce9021c3b
cambierelliot <elliot.cambier@edu.esiee.fr>
2024-11-15 13:54:55 +0100

    toute les pages

--- Api_WEB/FlaskApp/app.py
+++ Api_WEB/FlaskApp/app.py
@@ -8,5 +8,17 @@ es = Elasticsearch("http://elasticsearch:9200")
 def home():
     return render_template('home_page.html')
 
+@app.route("/aboutus")
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route("/restaurant")
+def restaurant():
+    return render_template('restaurant.html')
+
+@app.route("/analyse")
+def analyse():
+    return render_template('restaurant.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
