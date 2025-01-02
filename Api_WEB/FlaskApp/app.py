b3df3c53a08cbffe6bcad039bd00cecdb4704cdd
Brauxo <owen.braux@gmail.com>
2025-01-02 15:25:51 +0100

    test_new_ver_map

--- Api_WEB/FlaskApp/app.py
+++ Api_WEB/FlaskApp/app.py
@@ -35,6 +35,103 @@ DEPARTMENTS = {
     '971': 'Guadeloupe', '972': 'Martinique', '973': 'Guyane', '974': 'La Réunion', '976': 'Mayotte',
 }
 
+departements_coordinates = {
+    "01": {"name": "Ain", "coordinates": [5.8102, 46.01086]},
+    "02": {"name": "Aisne", "coordinates": [3.5583, 49.5594]},
+    "03": {"name": "Allier", "coordinates": [3.1883, 46.3936]},
+    "04": {"name": "Alpes-de-Haute-Provence", "coordinates": [6.2439, 44.1061]},
+    "05": {"name": "Hautes-Alpes", "coordinates": [6.2631, 44.6636]},
+    "06": {"name": "Alpes-Maritimes", "coordinates": [7.1164, 43.9375]},
+    "07": {"name": "Ardèche", "coordinates": [4.4247, 44.7517]},
+    "08": {"name": "Ardennes", "coordinates": [4.6408, 49.6156]},
+    "09": {"name": "Ariège", "coordinates": [1.5039, 42.9208]},
+    "10": {"name": "Aube", "coordinates": [4.1617, 48.3044]},
+    "11": {"name": "Aude", "coordinates": [2.4142, 43.1033]},
+    "12": {"name": "Aveyron", "coordinates": [2.6797, 44.2803]},
+    "13": {"name": "Bouches-du-Rhône", "coordinates": [5.0864, 43.5433]},
+    "14": {"name": "Calvados", "coordinates": [-0.3636, 49.0997]},
+    "15": {"name": "Cantal", "coordinates": [2.6686, 45.0511]},
+    "16": {"name": "Charente", "coordinates": [0.2017, 45.7181]},
+    "17": {"name": "Charente-Maritime", "coordinates": [-0.6744, 45.7808]},
+    "18": {"name": "Cher", "coordinates": [2.4911, 47.0647]},
+    "19": {"name": "Corrèze", "coordinates": [1.8769, 45.3569]},
+    "20": {"name": "Corse-du-Sud", "coordinates": [8.9881, 41.8636]},
+    "21": {"name": "Côte-d'Or", "coordinates": [4.7722, 47.4247]},
+    "22": {"name": "Côtes-d'Armor", "coordinates": [-2.8642, 48.4411]},
+    "23": {"name": "Creuse", "coordinates": [2.0189, 46.0903]},
+    "24": {"name": "Dordogne", "coordinates": [0.7414, 45.1042]},
+    "25": {"name": "Doubs", "coordinates": [6.3617, 47.1653]},
+    "26": {"name": "Drôme", "coordinates": [5.1681, 44.6842]},
+    "27": {"name": "Eure", "coordinates": [0.9961, 49.1136]},
+    "28": {"name": "Eure-et-Loir", "coordinates": [1.3703, 48.3875]},
+    "29": {"name": "Finistère", "coordinates": [-4.0589, 48.2611]},
+    "30": {"name": "Gard", "coordinates": [4.1803, 43.9933]},
+    "31": {"name": "Haute-Garonne", "coordinates": [1.1728, 43.3586]},
+    "32": {"name": "Gers", "coordinates": [0.4533, 43.6928]},
+    "33": {"name": "Gironde", "coordinates": [-0.5753, 44.8253]},
+    "34": {"name": "Hérault", "coordinates": [3.3672, 43.5797]},
+    "35": {"name": "Ille-et-Vilaine", "coordinates": [-1.6386, 48.1544]},
+    "36": {"name": "Indre", "coordinates": [1.5758, 46.7778]},
+    "37": {"name": "Indre-et-Loire", "coordinates": [0.6914, 47.2581]},
+    "38": {"name": "Isère", "coordinates": [5.5761, 45.2633]},
+    "39": {"name": "Jura", "coordinates": [5.6978, 46.7283]},
+    "40": {"name": "Landes", "coordinates": [-0.7839, 43.9656]},
+    "41": {"name": "Loir-et-Cher", "coordinates": [1.4294, 47.6167]},
+    "42": {"name": "Loire", "coordinates": [4.1658, 45.7264]},
+    "43": {"name": "Haute-Loire", "coordinates": [3.8064, 45.1281]},
+    "44": {"name": "Loire-Atlantique", "coordinates": [-1.6822, 47.3614]},
+    "45": {"name": "Loiret", "coordinates": [2.3442, 47.9120]},
+    "46": {"name": "Lot", "coordinates": [1.6047, 44.6242]},
+    "47": {"name": "Lot-et-Garonne", "coordinates": [0.4603, 44.3675]},
+    "48": {"name": "Lozère", "coordinates": [3.5003, 44.5172]},
+    "49": {"name": "Maine-et-Loire", "coordinates": [-0.5642, 47.3908]},
+    "50": {"name": "Manche", "coordinates": [-1.3275, 49.0794]},
+    "51": {"name": "Marne", "coordinates": [4.2386, 48.9492]},
+    "52": {"name": "Haute-Marne", "coordinates": [5.2264, 48.1094]},
+    "53": {"name": "Mayenne", "coordinates": [-0.6581, 48.1467]},
+    "54": {"name": "Meurthe-et-Moselle", "coordinates": [6.1650, 48.7869]},
+    "55": {"name": "Meuse", "coordinates": [5.3817, 48.9894]},
+    "56": {"name": "Morbihan", "coordinates": [-2.8100, 47.8464]},
+    "57": {"name": "Moselle", "coordinates": [6.6633, 49.0372]},
+    "58": {"name": "Nièvre", "coordinates": [3.5047, 47.1153]},
+    "59": {"name": "Nord", "coordinates": [3.2206, 50.4472]},
+    "60": {"name": "Oise", "coordinates": [2.4253, 49.4103]},
+    "61": {"name": "Orne", "coordinates": [0.1289, 48.6236]},
+    "62": {"name": "Pas-de-Calais", "coordinates": [2.2886, 50.4931]},
+    "63": {"name": "Puy-de-Dôme", "coordinates": [3.1417, 45.7258]},
+    "64": {"name": "Pyrénées-Atlantiques", "coordinates": [-0.7614, 43.2567]},
+    "65": {"name": "Hautes-Pyrénées", "coordinates": [0.1639, 43.0531]},
+    "66": {"name": "Pyrénées-Orientales", "coordinates": [2.5222, 42.6000]},
+    "67": {"name": "Bas-Rhin", "coordinates": [7.5514, 48.6708]},
+    "68": {"name": "Haut-Rhin", "coordinates": [7.2742, 47.8586]},
+    "69": {"name": "Rhône", "coordinates": [4.6414, 45.8703]},
+    "70": {"name": "Haute-Saône", "coordinates": [6.0861, 47.6411]},
+    "71": {"name": "Saône-et-Loire", "coordinates": [4.5422, 46.6447]},
+    "72": {"name": "Sarthe", "coordinates": [0.2222, 47.9944]},
+    "73": {"name": "Savoie", "coordinates": [6.4436, 45.4775]},
+    "74": {"name": "Haute-Savoie", "coordinates": [6.4281, 46.0344]},
+    "75": {"name": "Paris", "coordinates": [2.3422, 48.8567]},
+    "76": {"name": "Seine-Maritime", "coordinates": [1.0264, 49.6550]},
+    "77": {"name": "Seine-et-Marne", "coordinates": [2.9333, 48.6267]},
+    "78": {"name": "Yvelines", "coordinates": [1.8417, 48.8150]},
+    "79": {"name": "Deux-Sèvres", "coordinates": [-0.3172, 46.5556]},
+    "80": {"name": "Somme", "coordinates": [2.2778, 49.9581]},
+    "81": {"name": "Tarn", "coordinates": [2.1661, 43.7853]},
+    "82": {"name": "Tarn-et-Garonne", "coordinates": [1.2820, 44.0858]},
+    "83": {"name": "Var", "coordinates": [6.2181, 43.4606]},
+    "84": {"name": "Vaucluse", "coordinates": [5.1853, 43.9933]},
+    "85": {"name": "Vendée", "coordinates": [-1.2978, 46.6747]},
+    "86": {"name": "Vienne", "coordinates": [0.4603, 46.5639]},
+    "87": {"name": "Haute-Vienne", "coordinates": [1.2353, 45.8917]},
+    "88": {"name": "Vosges", "coordinates": [6.3806, 48.1967]},
+    "89": {"name": "Yonne", "coordinates": [3.5644, 47.8397]},
+    "90": {"name": "Territoire de Belfort", "coordinates": [6.9286, 47.6317]},
+    "91": {"name": "Essonne", "coordinates": [2.2431, 48.5222]},
+    "92": {"name": "Hauts-de-Seine", "coordinates": [2.2458, 48.8472]},
+    "93": {"name": "Seine-Saint-Denis", "coordinates": [2.4781, 48.9175]},
+    "94": {"name": "Val-de-Marne", "coordinates": [2.4689, 48.7775]},
+    "95": {"name": "Val-d'Oise", "coordinates": [2.1311, 49.0831]}
+}
 
 
 
@@ -159,11 +256,9 @@ def analyse():
     es = get_connection()
     index_name = "gaultmillau_restaurants"
 
-    # Récupére les paramètres de filtre, si fournis
     department = request.args.get('department', None)
-    selected_cuisines = request.args.getlist('cuisine')  # Liste des types de cuisine sélectionnés
+    selected_cuisines = request.args.getlist('cuisine')  
 
-    # Recupere tous les restaurants
     def get_all_restaurants(es, index_name, size=1000):
         all_hits = []
         from_index = 0
@@ -200,13 +295,13 @@ def analyse():
 
 
     data['department'] = data['address'].str[:2]
-    data = data[data['department'].fillna('0').str.isdigit()]  # Filtrer les départements valides
+    data = data[data['department'].fillna('0').str.isdigit()] 
 
     data['department_name'] = data['department'].map(DEPARTMENTS)
 
     data['rating'] = data['rating'].apply(lambda x: float(x) if isinstance(x, (int, float)) else None)
-    data = data[data['rating'].notna()]  # Supprimer les valeurs manquantes
-    data['rating'] = data['rating'].apply(lambda x: max(5, min(20, x)))  # Assurez-vous que les notes sont entre 5 et 20
+    data = data[data['rating'].notna()]  
+    data['rating'] = data['rating'].apply(lambda x: max(5, min(20, x)))  
 
     dept_counts = data['department_name'].value_counts().reset_index()
     dept_counts.columns = ['department', 'count']
@@ -243,7 +338,6 @@ def create_geojson(dept_counts):
     with open('FlaskApp/static/departements.geojson', 'r') as geojson_file:
         geojson_template = json.load(geojson_file)
     
-    # Map les données de counts sur les propriétés GeoJSON
     for feature in geojson_template['features']:
         department_code = feature['properties']['code']
         feature['properties']['restaurant_count'] = int(
